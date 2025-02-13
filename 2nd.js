c7cc87725af4ee0bf06707f4579a3d6100be4f6f
Ryan Joshua Velarde <rjvelarde0105v@gmail.com>
2025-02-13 23:01:07 +0800

    hi aj

--- 2nd.js
+++ 2nd.js
@@ -25,31 +25,43 @@ shuffledThings.forEach((letter) => {
     letter.classList.add("center");
   }
   let offsetX, offsetY;
-  letter.addEventListener("mousedown", (e) => {
-    if (e.target.tagName !== "BUTTON") {
-      const rect = e.target.getBoundingClientRect();
-
-      letter.style.position = "fixed";
-      letter.style.left = `${rect.left}px`;
-      letter.style.top = `${rect.top}px`;
-
-      offsetX = e.clientX - rect.left;
-      offsetY = e.clientY - rect.top;
-
-      letter.style.zIndex = zIndexCounter++;
-      const moveAt = (posX, posY) => {
-        letter.style.left = `${posX - offsetX}px`;
-        letter.style.top = `${posY - offsetY}px`;
-      };
-      const onMouseMove = (moveEvent) => moveAt(moveEvent.clientX, moveEvent.clientY);
-      const onMouseUp = () => {
-        document.removeEventListener("mousemove", onMouseMove);
-        document.removeEventListener("mouseup", onMouseUp);
-      };
-      document.addEventListener("mousemove", onMouseMove);
-      document.addEventListener("mouseup", onMouseUp);
-    }
-  });
+  const startDrag = (e) => {
+    e.preventDefault();
+    
+    const rect = letter.getBoundingClientRect();
+    letter.style.position = "fixed";
+    letter.style.left = `${rect.left}px`;
+    letter.style.top = `${rect.top}px`;
+  
+    offsetX = (e.clientX || e.touches[0].clientX) - rect.left;
+    offsetY = (e.clientY || e.touches[0].clientY) - rect.top;
+    letter.style.zIndex = zIndexCounter++;
+  
+    const moveAt = (posX, posY) => {
+      letter.style.left = `${posX - offsetX}px`;
+      letter.style.top = `${posY - offsetY}px`;
+    };
+  
+    const onMove = (moveEvent) => {
+      moveAt(moveEvent.clientX || moveEvent.touches[0].clientX, moveEvent.clientY || moveEvent.touches[0].clientY);
+    };
+  
+    const onEnd = () => {
+      document.removeEventListener("mousemove", onMove);
+      document.removeEventListener("mouseup", onEnd);
+      document.removeEventListener("touchmove", onMove);
+      document.removeEventListener("touchend", onEnd);
+    };
+  
+    document.addEventListener("mousemove", onMove);
+    document.addEventListener("mouseup", onEnd);
+    document.addEventListener("touchmove", onMove, { passive: false });
+    document.addEventListener("touchend", onEnd);
+  };
+  
+  letter.addEventListener("mousedown", startDrag);
+  letter.addEventListener("touchstart", startDrag, { passive: false });
+  
 });
 document.querySelector("#openEnvelope").addEventListener("click", () => {
   document.querySelector(".envelope").classList.add("active");
